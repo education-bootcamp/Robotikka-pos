e3ef021858ecc09f6c545e72471aa224bcc49f77
ozen-hub <info.ozenhub@gmail.com>
2023-05-08 22:33:46 +0530

    Initial commit

---
+++ src/com/devstack/pos/Initialize.java
@@ -0,0 +1,16 @@
+package com.devstack.pos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Initialize extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
