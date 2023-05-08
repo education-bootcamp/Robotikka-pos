822a87c0baebdd63733ff9b91662fbf09305147d
ozen-hub <info.ozenhub@gmail.com>
2023-05-08 23:23:29 +0530

    created sign up without encryption

--- src/com/devstack/pos/controller/SignupFormController.java
+++ src/com/devstack/pos/controller/SignupFormController.java
@@ -5,10 +5,15 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -20,6 +25,27 @@ public class SignupFormController {
     }
 
     public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                    "root","1234");
+            String sql="INSERT INTO user VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, txtEmail.getText());
+            preparedStatement.setString(2, textPassword.getText());
+
+           if (preparedStatement.executeUpdate()>0){
+               new Alert(Alert.AlertType.CONFIRMATION, "User Saved!").show();
+           }else{
+               new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+           }
+
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
     }
 
     private void setUi(String url) throws IOException {
