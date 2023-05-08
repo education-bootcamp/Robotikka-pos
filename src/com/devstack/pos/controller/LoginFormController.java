543cc7731c3ca446548d1755e2c26513a397a4a3
ozen-hub <info.ozenhub@gmail.com>
2023-05-08 23:49:46 +0530

    Completed the login part

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,14 +1,17 @@
 package com.devstack.pos.controller;
 
+import com.devstack.pos.util.PasswordManager;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -20,13 +23,39 @@ public class LoginFormController {
     }
 
     public void btnSignInOnAction(ActionEvent actionEvent) {
-        // login
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                    "root", "1234");
+            String sql = "SELECT * FROM user WHERE email=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, txtEmail.getText());
+
+            ResultSet set = preparedStatement.executeQuery();
+            if (set.next()) {
+                if (PasswordManager.checkPassword(txtPassword.getText(), set.getString("password"))) {
+                    System.out.println("Completed");
+                } else {
+                    new Alert(Alert.AlertType.WARNING, "check your password and try again!").show();
+                }
+            } else {
+                new Alert(Alert.AlertType.WARNING, "User email not found!").show();
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+
     }
 
     private void setUi(String url) throws IOException {
-        Stage stage = (Stage)context.getScene().getWindow();
+        Stage stage = (Stage) context.getScene().getWindow();
         stage.setScene(
-                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")))
         );
         stage.centerOnScreen();
     }
--- src/com/devstack/pos/util/PasswordManager.java
+++ src/com/devstack/pos/util/PasswordManager.java
@@ -7,4 +7,7 @@ public class PasswordManager {
     public static String encryptPassword(String plainText){
         return BCrypt.hashpw(plainText,BCrypt.gensalt(10));
     }
+    public static boolean checkPassword(String plainText,String hash){
+        return BCrypt.checkpw(plainText,hash);
+    }
 }
