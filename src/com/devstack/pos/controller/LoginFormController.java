f1c8d259701a2c3a58e9e502797ea15fa83485e1
ozen-hub <info.ozenhub@gmail.com>
2023-05-08 23:02:32 +0530

    finalize login & sign up ui

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,4 +1,34 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+        // login
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage)context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+    }
+
 }
--- src/com/devstack/pos/controller/SignupFormController.java
+++ src/com/devstack/pos/controller/SignupFormController.java
@@ -1,4 +1,32 @@
 package com.devstack.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignupFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField textPassword;
+
+    public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+
+    public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage)context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+    }
 }
