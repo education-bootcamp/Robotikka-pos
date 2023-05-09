ef30d6d69b3aa307b6771edfa1b0ce729a6daf30
ozen-hub <info.ozenhub@gmail.com>
2023-05-09 21:40:00 +0530

    created customer save process

--- src/com/devstack/pos/controller/CustomerFormController.java
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -1,12 +1,16 @@
 package com.devstack.pos.controller;
 
+import com.devstack.pos.dao.DatabaseAccessCode;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.SQLException;
 
 public class CustomerFormController {
 
@@ -26,5 +30,29 @@ public class CustomerFormController {
     public TableColumn colOperate;
 
     public void btnSaveUpdateOnAction(ActionEvent actionEvent) {
+        try{
+            if (
+                    DatabaseAccessCode.createCustomer(
+                            txtEmail.getText(),txtName.getText(),
+                            txtContact.getText(),Double.parseDouble(txtSalary.getText())
+                    )
+            ){
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+                clearFields();
+            }else{
+                new Alert(Alert.AlertType.WARNING, "Try Again!").show();
+            }
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+    }
+
+    private void clearFields() {
+        txtEmail.clear();
+        txtName.clear();
+        txtContact.clear();
+        txtSalary.clear();
     }
 }
--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -44,8 +44,17 @@ public class DatabaseAccessCode {
     //====User management===============
 
     //====Customer management===============
-    public static boolean createCustomer(String email,String name, String contact, double salary){
-        return false;
+    public static boolean createCustomer(String email,String name, String contact, double salary) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root","1234");
+        String sql="INSERT INTO customer VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, contact);
+        preparedStatement.setDouble(4, salary);
+        return preparedStatement.executeUpdate()>0;
     }
     public static boolean updateCustomer(String email,String name, String contact, double salary){
         return false;
