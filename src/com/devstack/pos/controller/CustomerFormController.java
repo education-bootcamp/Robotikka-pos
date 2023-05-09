ae3a094a9612ca08407484b1915b5b50515c5ee4
ozen-hub <info.ozenhub@gmail.com>
2023-05-09 22:19:25 +0530

    customer loading part completed

--- src/com/devstack/pos/controller/CustomerFormController.java
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -1,15 +1,21 @@
 package com.devstack.pos.controller;
 
 import com.devstack.pos.dao.DatabaseAccessCode;
+import com.devstack.pos.dto.CustomerDto;
+import com.devstack.pos.view.tm.CustomerTm;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class CustomerFormController {
@@ -21,7 +27,7 @@ public class CustomerFormController {
     public JFXTextField txtSalary;
     public JFXButton btnSaveUpdate;
     public TextField txtSearch;
-    public TableView tbl;
+    public TableView<CustomerTm> tbl;
     public TableColumn colId;
     public TableColumn colEmail;
     public TableColumn colName;
@@ -29,6 +35,33 @@ public class CustomerFormController {
     public TableColumn colSalary;
     public TableColumn colOperate;
 
+    private String searchText="";
+
+    public void initialize() throws SQLException, ClassNotFoundException {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colOperate.setCellValueFactory(new PropertyValueFactory<>("deleteButton"));
+
+        loadAllCustomers(searchText);
+    }
+
+    private void loadAllCustomers(String searchText) throws SQLException, ClassNotFoundException {
+        ObservableList<CustomerTm> observableList = FXCollections.observableArrayList();
+        int counter=1;
+        for (CustomerDto dto: DatabaseAccessCode.searchCustomers(searchText)){
+            Button btn = new Button("Delete");
+            CustomerTm tm = new CustomerTm(
+                    counter,dto.getEmail(), dto.getName(), dto.getContact(), dto.getSalary(),
+                    btn
+            );
+            observableList.add(tm);
+            counter++;
+        }
+        tbl.setItems(observableList);
+    }
     public void btnSaveUpdateOnAction(ActionEvent actionEvent) {
         try{
             if (
@@ -39,6 +72,7 @@ public class CustomerFormController {
             ){
                 new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
                 clearFields();
+                loadAllCustomers(searchText);
             }else{
                 new Alert(Alert.AlertType.WARNING, "Try Again!").show();
             }
@@ -54,5 +88,18 @@ public class CustomerFormController {
         txtName.clear();
         txtContact.clear();
         txtSalary.clear();
+    }
+
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.centerOnScreen();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")))
+        );
+    }
+
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
     }
 }
--- src/com/devstack/pos/controller/DashboardFormController.java
+++ src/com/devstack/pos/controller/DashboardFormController.java
@@ -1,12 +1,18 @@
 package com.devstack.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
     public AnchorPane context;
 
-    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("CustomerForm");
     }
 
     public void btnProductOnActions(ActionEvent actionEvent) {
@@ -19,5 +25,13 @@ public class DashboardFormController {
     }
 
     public void btnIncomeReportOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.centerOnScreen();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/" + url + ".fxml")))
+        );
     }
 }
