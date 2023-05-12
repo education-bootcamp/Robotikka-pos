38ab6848a7f46f8e449712b835be44f0815bc95a
ozen-hub <info.ozenhub@gmail.com>
2023-05-12 21:29:54 +0530

    created customer package

--- src/com/devstack/pos/dao/custom/ProductDao.java
+++ src/com/devstack/pos/dao/custom/ProductDao.java
@@ -1,4 +1,14 @@
 package com.devstack.pos.dao.custom;
 
+import com.devstack.pos.entity.Customer;
+import com.devstack.pos.entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+    public boolean saveProduct(Product product);
+    public boolean updateProduct(Product product);
+    public boolean deleteProduct(int code);
+    public Product findProduct(int code);
+    public List<Product> findAllProducts();
 }
--- src/com/devstack/pos/dao/custom/UserDao.java
+++ src/com/devstack/pos/dao/custom/UserDao.java
@@ -1,4 +1,14 @@
 package com.devstack.pos.dao.custom;
 
+import com.devstack.pos.entity.Customer;
+import com.devstack.pos.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+    public boolean saveUser(User user);
+    public boolean updateUser(User user);
+    public boolean deleteUser(String email);
+    public User findUser(String email);
+    public List<User> findAllUsers();
 }
