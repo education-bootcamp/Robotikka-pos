0eee439ddce68aa9addab0db8ddf7a8c4d096bd8
ozen-hub <info.ozenhub@gmail.com>
2023-05-14 16:40:00 +0530

    10

--- src/com/devstack/pos/controller/PlaceOrderFormController.java
+++ src/com/devstack/pos/controller/PlaceOrderFormController.java
@@ -1,15 +1,33 @@
 package com.devstack.pos.controller;
 
+import com.devstack.pos.bo.BoFactory;
+import com.devstack.pos.bo.custom.CustomerBo;
+import com.devstack.pos.dto.CustomerDto;
+import com.devstack.pos.enums.BoType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class PlaceOrderFormController {
     public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtName;
+    public Hyperlink urlNewLoyalty;
+    public Label lblLoyaltyType;
+    public TextField txtContact;
+    public TextField txtSalary;
+
+    CustomerBo bo = BoFactory.getInstance().getBo(BoType.CUSTOMER);
+
 
     public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashboardForm", false);
@@ -39,5 +57,35 @@ public class PlaceOrderFormController {
             stage.centerOnScreen();
         }
 
+    }
+
+    public void searchCustomer(ActionEvent actionEvent) {
+        try {
+
+            CustomerDto customer = bo.findCustomer(txtEmail.getText());
+            if (customer!=null){
+
+                txtName.setText(customer.getName());
+                txtSalary.setText(String.valueOf(customer.getSalary()));
+                txtContact.setText(customer.getContact());
+
+                fetchLoyaltyCardData(txtEmail.getText());
+
+            }else{
+                new Alert(Alert.AlertType.WARNING, "Can't Find the Customer!").show();
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.WARNING, "Can't Find the Customer!").show();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void fetchLoyaltyCardData(String email) {
+        urlNewLoyalty.setText("+ New Loyalty");
+        urlNewLoyalty.setVisible(true);
+    }
+
+    public void newLoyaltyOnAction(ActionEvent actionEvent) {
     }
 }
