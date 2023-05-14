6f21221747c79e75d8f8a142f3d690086fce3ffd
ozen-hub <info.ozenhub@gmail.com>
2023-05-14 17:47:32 +0530

    12

--- src/com/devstack/pos/controller/PlaceOrderFormController.java
+++ src/com/devstack/pos/controller/PlaceOrderFormController.java
@@ -6,13 +6,14 @@ import com.devstack.pos.bo.custom.ProductDetailBo;
 import com.devstack.pos.dto.CustomerDto;
 import com.devstack.pos.dto.ProductDetailJoinDto;
 import com.devstack.pos.enums.BoType;
+import com.devstack.pos.view.tm.CartTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -36,11 +37,30 @@ public class PlaceOrderFormController {
     public Label lblDiscountAvl;
     public TextField txtBuyingPrice;
     public TextField txtQty;
+    public TableView<CartTm> tblCart;
+    public TableColumn colCode;
+    public TableColumn colDesc;
+    public TableColumn colSelPrice;
+    public TableColumn colSelDisc;
+    public TableColumn colSelShPrice;
+    public TableColumn colSelQty;
+    public TableColumn colSelTotal;
+    public TableColumn colSelOperation;
 
     CustomerBo bo = BoFactory.getInstance().getBo(BoType.CUSTOMER);
     private ProductDetailBo productDetailBo = BoFactory.getInstance().getBo(BoType.PRODUCT_DETAIL);
 
 
+    public void initialize(){
+        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDesc.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colSelPrice.setCellValueFactory(new PropertyValueFactory<>("sellingPrice"));
+        colSelDisc.setCellValueFactory(new PropertyValueFactory<>("discount"));
+        colSelShPrice.setCellValueFactory(new PropertyValueFactory<>("showPrice"));
+        colSelQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colSelTotal.setCellValueFactory(new PropertyValueFactory<>("totalCost"));
+        colSelOperation.setCellValueFactory(new PropertyValueFactory<>("btn"));
+    }
 
     public void btnBackToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("DashboardForm", false);
@@ -117,5 +137,26 @@ public class PlaceOrderFormController {
             throw new RuntimeException(e);
         }
 
+    }
+
+    ObservableList<CartTm> tms= FXCollections.observableArrayList();
+    public void addToCart(ActionEvent actionEvent) {
+        int qty=Integer.parseInt(txtQty.getText());
+        double sellingPrice= Double.parseDouble(txtSellingPrice.getText());
+        double totalCost = qty*sellingPrice;
+        Button btn = new Button("Remove");
+
+        CartTm tm = new CartTm(txtBarcode.getText(),
+                txtDescription.getText(),
+                Double.parseDouble(txtDiscount.getText()),
+                sellingPrice,
+                Double.parseDouble(txtShowPrice.getText()),
+                qty,
+                totalCost,
+                btn);
+        tms.add(tm);
+
+
+        tblCart.setItems(tms);
     }
 }
